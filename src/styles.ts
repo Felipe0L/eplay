0b799d9ce2c3bde498ae9bb380e60c4140f2d912
Felipe0l <felipeolivel@hotmail.com>
2024-09-13 12:08:22 -0300

    Construindo o cabeçalho

--- src/styles.ts
+++ src/styles.ts
@@ -1,6 +1,6 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, styled } from 'styled-components'
 
-const cores = {
+export const cores = {
   branca: '#EEEEEE',
   preta: '#111',
   cinza: '#333',
@@ -13,9 +13,16 @@ export const GlobalCss = createGlobalStyle`
   padding: 0;
   box-sizzing: border-box;
   font-family: Roboto, sans-serif;
+  list-style: none;
 }
   body {
     background-color: ${cores.preta};
     color: ${cores.branca};
+    padding-top: 40px;
   }
+`
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
 `
