03ad5bfacbefa0ffa8e3909907c6d832f8b1c358
Felipe0l <felipeolivel@hotmail.com>
2024-10-02 14:56:55 -0300

    fazendo melhorias no código parte 2

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,19 +1,13 @@
-import { Game } from '../../pages/Home'
 import Product from '../Product'
-import { Container, List } from './styles'
+import { Game } from '../../pages/Home'
+import { parseToBrl } from '../../utils'
+import * as S from './styles'
 
 export type Props = {
   title: string
   background: 'gray' | 'black'
   games: Game[]
   id?: string
-}
-
-export const formataPreco = (preco = 0) => {
-  return new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL'
-  }).format(preco)
 }
 
 const ProductsList = ({ title, background, games, id }: Props) => {
@@ -27,16 +21,16 @@ const ProductsList = ({ title, background, games, id }: Props) => {
       tags.push(`${game.prices.discount}%`)
     }
     if (game.prices.current) {
-      tags.push(formataPreco(game.prices.current))
+      tags.push(parseToBrl(game.prices.current))
     }
     return tags
   }
 
   return (
-    <Container id={id} background={background}>
+    <S.Container id={id} background={background}>
       <div className="container">
         <h2>{title}</h2>
-        <List>
+        <S.List>
           {games.map((game) => (
             <li key={game.id}>
               <Product
@@ -50,9 +44,9 @@ const ProductsList = ({ title, background, games, id }: Props) => {
               />
             </li>
           ))}
-        </List>
+        </S.List>
       </div>
-    </Container>
+    </S.Container>
   )
 }
 
